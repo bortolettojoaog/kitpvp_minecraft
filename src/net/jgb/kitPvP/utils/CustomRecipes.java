eec1177880b7ee07bdb0d34a553e817338882226
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-01 21:50:55 -0300

    feat: create custom item method

--- src/net/jgb/kitPvP/utils/CustomRecipes.java
+++ src/net/jgb/kitPvP/utils/CustomRecipes.java
@@ -1,9 +1,14 @@
 package net.jgb.kitPvP.utils;
 
+import java.util.List;
+import java.util.Map;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapelessRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class CustomRecipes {
 
@@ -22,6 +27,38 @@ public class CustomRecipes {
         recipe.addIngredient(material2.getNewData((byte)dataMaterial2));
 
         Bukkit.addRecipe(recipe);
+    }
+    
+    public static ItemStack createItem(Material material, int amount, String displayName, List<String> lore) {
+    	ItemStack itemStack = new ItemStack(material, amount);
+    	ItemMeta itemMeta = itemStack.getItemMeta();
+    	itemMeta.setDisplayName(displayName);
+    	itemMeta.setLore(lore);
+    	itemStack.setItemMeta(itemMeta);
+    	
+    	return itemStack;
+    }
+    
+    public static ItemStack createItem(Material material, int amount, String displayName, List<String> lore, int durability) {
+    	ItemStack itemStack = createItem(material, amount, displayName, lore);
+    	ItemMeta itemMeta = itemStack.getItemMeta();
+    	itemStack.setDurability((short)durability);
+    	itemStack.setItemMeta(itemMeta);
+    	
+    	return itemStack;
+    }
+    
+    public static ItemStack createItem(Material material, int amount, String displayName, List<String> lore, short durability, Map<Enchantment, Integer> enchantments) {
+    	ItemStack itemStack = createItem(material, amount, displayName, lore, durability);
+    	ItemMeta itemMeta = itemStack.getItemMeta();
+    	for (Map.Entry<Enchantment, Integer> entry : enchantments.entrySet()) {
+            Enchantment enchantment = entry.getKey();
+            int level = entry.getValue();
+            itemMeta.addEnchant(enchantment, level, false);
+        }
+    	itemStack.setItemMeta(itemMeta);
+    	
+    	return itemStack;
     }
 
 }
