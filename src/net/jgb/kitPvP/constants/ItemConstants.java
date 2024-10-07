c80960bb20cb22739e82ef5eba8b33e22eb82bbb
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-07 15:02:00 -0300

    feat: create constants

---
+++ src/net/jgb/kitPvP/constants/ItemConstants.java
@@ -0,0 +1,7 @@
+package net.jgb.kitPvP.constants;
+
+public class ItemConstants {
+	
+	public static String[] INTERACTIVE_ITEMS = new String[] {"SWORD", "HELMET", "CHESTPLATE", "LEGGINGS", "BOOTS", "MUSHROOM", "BOWL"};
+
+}
