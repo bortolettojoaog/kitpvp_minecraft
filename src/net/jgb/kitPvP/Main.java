e748bf82f658280f72707f8794e89ae8e9d19550
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-08 13:10:00 -0300

    refactor: create setter to utilities orchestrator

--- src/net/jgb/kitPvP/Main.java
+++ src/net/jgb/kitPvP/Main.java
@@ -1,8 +1,8 @@
 package net.jgb.kitPvP;
 
 import net.jgb.kitPvP.events.Listeners;
-import net.jgb.kitPvP.utils.CustomRecipes;
-import net.jgb.kitPvP.utils.Message;
+import net.jgb.kitPvP.utils.Utils;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,34 +10,35 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
 
     private static Main currentPlugin;
-    private static Message currentMessage;
+    private static Utils utils;
 
     @Override
     public void onLoad() {
         setPlugin(this);
-        setMessage(new Message());
+        setUtils(new Utils());
 
-        Bukkit.getConsoleSender().sendMessage(getMessage().getInformationPrefix().replace("{symbol}", "§6§l!") +  "§7plugin loading...");
+        Bukkit.getConsoleSender().sendMessage(utils.messageUtils().getInformationPrefix() +  " §7plugin loading...");
     }
 
     @Override
     public void onEnable() {
-        Bukkit.getConsoleSender().sendMessage(getMessage().getSucessPrefix().replace("{symbol}", "§2§l!") + " §7plugin successfully activated...");
+        Bukkit.getConsoleSender().sendMessage(utils.messageUtils().getSucessPrefix() + " §7plugin successfully activated...");
         init();
     }
 
     @Override
     public void onDisable() {
         setPlugin(null);
-        Bukkit.getConsoleSender().sendMessage(getMessage().getErrorPrefix().replace("{symbol}", "§4§l!") + " §7Plugin successfully disabled...");
+        Bukkit.getConsoleSender().sendMessage(utils.messageUtils().getErrorPrefix() + " §7Plugin successfully disabled...");
     }
 
     public void init() {
         new Listeners();
 
-        CustomRecipes.soupRecipe(Material.RED_MUSHROOM, Material.BROWN_MUSHROOM, Material.BOWL);
-        CustomRecipes.soupRecipe(Material.CACTUS, Material.CACTUS, Material.BOWL);
-        CustomRecipes.soupRecipe(Material.INK_SACK, Material.BOWL, 3, 0);
+        utils.customRecipeUtils().soupRecipe(Material.RED_MUSHROOM, Material.BROWN_MUSHROOM, Material.BOWL);
+        utils.customRecipeUtils().soupRecipe(Material.CACTUS, Material.CACTUS, Material.BOWL);
+        utils.customRecipeUtils().soupRecipe(Material.INK_SACK, Material.BOWL, 3, 0);
+        utils.customRecipeUtils().soupRecipe(Material.PUMPKIN_SEEDS, Material.PUMPKIN_SEEDS, Material.BOWL);
     }
 
     public static Main getPlugin() {
@@ -48,11 +49,11 @@ public class Main extends JavaPlugin {
     	currentPlugin = main;
     }
 
-    public static Message getMessage() {
-        return currentMessage;
+    public static Utils getUtils() {
+        return utils;
     }
-
-    private void setMessage(Message message) {
-    	currentMessage = message;
+    
+    private void setUtils(Utils utilsClass) {
+    	utils = utilsClass;
     }
 }
