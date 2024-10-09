75894843f14d8c198ee1003a785d5d345e003cc1
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-09 08:30:45 -0300

    refactor: change item enum to have more information

--- src/net/jgb/kitPvP/enums/ItemEnum.java
+++ src/net/jgb/kitPvP/enums/ItemEnum.java
@@ -1,18 +1,26 @@
 package net.jgb.kitPvP.enums;
 
+import org.bukkit.Material;
+
 public enum ItemEnum {
 	
-	EMERALD("Roleta da Sorte"),
-	CHEST("Menu"),
-	SWORD("Treinamento");
+	BOX("Roleta da Sorte", Material.EMERALD),
+	KIT("Menu", Material.CHEST),
+	TRAIN("Treinamento", Material.DIAMOND_SWORD);
 	
 	String displayName;
+	Material material;
 	
-	ItemEnum(String displayName) {
+	ItemEnum(String displayName, Material material) {
 		this.displayName = displayName;
+		this.material = material;
 	}
 	
 	public String getDisplayName() {
 		return this.displayName;
+	}
+	
+	public Material getMaterial() {
+		return this.material;
 	}
 }
