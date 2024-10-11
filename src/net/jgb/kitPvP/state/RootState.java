cc2c75a8395e69d1f7e18592350e921ce37514d1
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-10 22:46:52 -0300

    feat: create methods to consume root state

--- src/net/jgb/kitPvP/state/RootState.java
+++ src/net/jgb/kitPvP/state/RootState.java
@@ -3,10 +3,38 @@ package net.jgb.kitPvP.state;
 import java.util.HashMap;
 import java.util.UUID;
 
+import org.bukkit.entity.Player;
+
 import net.jgb.kitPvP.enums.PlayerModeEnum;
 
 public class RootState {
 	
-	public static HashMap<UUID, PlayerModeEnum> players_mode = new HashMap<>();
+	private HashMap<UUID, PlayerModeEnum> players_mode;
+	
+	public RootState() {
+		this.players_mode = new HashMap<>();
+	}
 
+	public void clean() {
+		players_mode.clear();
+	}
+	
+	public boolean comparePlayerMode(Player player, PlayerModeEnum playerMode) {
+		return this.players_mode.containsKey(player.getUniqueId()) && this.players_mode.get(player.getUniqueId()).equals(playerMode);
+	}
+	
+	public void updatePlayerMode(Player player, PlayerModeEnum playerMode) {
+		this.players_mode.put(player.getUniqueId(), playerMode);
+	}
+	
+	public void resetPlayerMode(Player player) {
+		this.players_mode.put(player.getUniqueId(), PlayerModeEnum.HOLDING);
+	}
+	
+	public PlayerModeEnum getPlayerMode(Player player) {
+		if (!this.players_mode.containsKey(player.getUniqueId()))
+			updatePlayerMode(player, PlayerModeEnum.HOLDING);
+		
+		return this.players_mode.get(player.getUniqueId());
+	}
 }
