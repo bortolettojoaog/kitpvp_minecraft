efe68d922d46bc99fa86297f5797cc1a1f4b4a24
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-10 22:23:44 -0300

    feat: create player constants

---
+++ src/net/jgb/kitPvP/constants/PlayerConstants.java
@@ -0,0 +1,9 @@
+package net.jgb.kitPvP.constants;
+
+public class PlayerConstants {
+
+	public static Integer MAX_LIFE_LEVEL = 20;
+	public static Integer MAX_FOOD_LEVEL = 20;
+	public static Integer SOUP_REGENETATION = 7;
+	public static String BUILD_PERMISSION = "kitpvp.build";
+}
