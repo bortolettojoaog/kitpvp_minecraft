977d14e38c7bbf2158ae3b065851913bb3f65d3d
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-15 12:32:26 -0300

    feat: create minimum y map constant

--- src/net/jgb/kitPvP/constants/PlayerConstants.java
+++ src/net/jgb/kitPvP/constants/PlayerConstants.java
@@ -6,4 +6,5 @@ public class PlayerConstants {
 	public static Integer MAX_FOOD_LEVEL = 20;
 	public static Integer SOUP_REGENETATION = 7;
 	public static String BUILD_PERMISSION = "kitpvp.build";
+	public static Integer MINIMUM_Y_MAP = 50;
 }
