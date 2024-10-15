5d60cbab622896edd55542ddfbfe5d43d31d0584
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-15 09:46:29 -0300

    feat: create inventory constants

---
+++ src/net/jgb/kitPvP/constants/InventoryConstants.java
@@ -0,0 +1,14 @@
+package net.jgb.kitPvP.constants;
+
+import java.util.Arrays;
+import java.util.List;
+
+import net.jgb.kitPvP.utils.CustomInventory;
+
+public class InventoryConstants {
+	
+	public static List<Integer> MIDDLE_INVENTORY = Arrays.asList(11, 12, 13, 14, 15, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30, 31, 32, 33, 34, 38, 39, 40, 41, 42);
+
+	public static CustomInventory MENU_INVENTORY = new CustomInventory("§e§lMenu", 54);
+	public static CustomInventory BOX_INVENTORY = new CustomInventory("§e§lRoleta", 54);
+}
