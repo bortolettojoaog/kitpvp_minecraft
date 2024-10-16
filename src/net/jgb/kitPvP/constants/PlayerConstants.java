5eb1204ae8e9c612dea2ac2292996c2220bf533f
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-16 15:55:30 -0300

    feat: create warp command permissions

--- src/net/jgb/kitPvP/constants/PlayerConstants.java
+++ src/net/jgb/kitPvP/constants/PlayerConstants.java
@@ -6,5 +6,8 @@ public class PlayerConstants {
 	public static Integer MAX_FOOD_LEVEL = 20;
 	public static Integer SOUP_REGENETATION = 7;
 	public static String BUILD_PERMISSION = "kitpvp.build";
+	public static String SET_WARP_PERMISSION = "kitpvp.set.warp.command";
+	public static String UNSET_WARP_PERMISSION = "kitpvp.unset.warp.command";
+	public static String CONFIG_WARP_PERMISSION = "kitpvp.config.warp.command";
 	public static Integer MINIMUM_Y_MAP = 50;
 }
