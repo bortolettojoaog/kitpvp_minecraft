4cf8c0e17be75ca117af236002799281afac08f4
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-17 20:58:03 -0300

    fix: change warp config

--- src/net/jgb/kitPvP/configs/warp/warpsConfig.java
+++ src/net/jgb/kitPvP/configs/warp/warpsConfig.java
@@ -1,4 +1,6 @@
-package net.jgb.kitPvP.configs;
+package net.jgb.kitPvP.configs.warp;
+
+import net.jgb.kitPvP.configs.Config;
 
 public class warpsConfig extends Config {
 
@@ -10,9 +12,10 @@ public class warpsConfig extends Config {
 	public void createConfig() {
 		if (!this.getConfig().contains("Warps")) {
 			this.getConfig().createSection("Warps");
-			this.getConfig().createSection("Warps.World");
+			this.getConfig().createSection("Warps.Normal");
 			this.getConfig().createSection("Warps.Nether");
 			this.getConfig().createSection("Warps.TheEnd");
+			this.getConfig().createSection("Warps.Unknown");
 		}
 	}
 }
