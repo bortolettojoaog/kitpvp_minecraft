54223fec19de3f0cad4e80ef3d9853e5d10a5d34
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-20 15:03:41 -0300

    feat: add new events to be register

--- src/net/jgb/kitPvP/events/Listeners.java
+++ src/net/jgb/kitPvP/events/Listeners.java
@@ -1,10 +1,12 @@
 package net.jgb.kitPvP.events;
 
 import net.jgb.kitPvP.Main;
+import net.jgb.kitPvP.events.inventories.WarpConfigInventory;
 
 public class Listeners {
 
     public Listeners() {
-        Main.getPlugin().getServer().getPluginManager().registerEvents(new PlayerInteract(), Main.getPlugin());
+        Main.getPlugin().getServer().getPluginManager().registerEvents(new DefaultEvents(), Main.getPlugin());
+        Main.getPlugin().getServer().getPluginManager().registerEvents(new WarpConfigInventory(), Main.getPlugin());
     }
 }
