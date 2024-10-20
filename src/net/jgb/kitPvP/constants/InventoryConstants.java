889809d96e79ddccc9df21159f7f2ddb009dad46
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-20 15:02:45 -0300

    refactor: change constant names

--- src/net/jgb/kitPvP/constants/InventoryConstants.java
+++ src/net/jgb/kitPvP/constants/InventoryConstants.java
@@ -3,12 +3,14 @@ package net.jgb.kitPvP.constants;
 import java.util.Arrays;
 import java.util.List;
 
-import net.jgb.kitPvP.utils.customs.CustomInventory;
+import net.jgb.kitPvP.utils.customs.CustomPaginatedInventory;
 
 public class InventoryConstants {
 	
 	public static List<Integer> MIDDLE_INVENTORY = Arrays.asList(11, 12, 13, 14, 15, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30, 31, 32, 33, 34, 38, 39, 40, 41, 42);
 
-	public static CustomInventory MENU_INVENTORY = new CustomInventory("§e§lMenu", 54);
-	public static CustomInventory BOX_INVENTORY = new CustomInventory("§e§lRoleta", 54);
+	public static CustomPaginatedInventory MENU_INVENTORY = new CustomPaginatedInventory("§e§lSelection Kit Menu", 54);
+	public static CustomPaginatedInventory BOX_INVENTORY = new CustomPaginatedInventory("§e§lMystery Box", 54);
+	
+	public static String CONFIG_WARP_INVENTORY = "§e§lWARP CONFIG";
 }
