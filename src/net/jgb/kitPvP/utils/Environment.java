2ff5f96dd42022580bbb7bce323180f47a277629
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-20 14:59:59 -0300

    refactor: change the world options

--- src/net/jgb/kitPvP/utils/Environment.java
+++ src/net/jgb/kitPvP/utils/Environment.java
@@ -3,20 +3,22 @@ package net.jgb.kitPvP.utils;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
+import net.jgb.kitPvP.enums.WorldEnum;
+
 public class Environment {
 	
-	public String checkPlayerWorld(Player player) {
+	public WorldEnum checkPlayerWorld(Player player) {
         World world = player.getWorld();
         World.Environment environment = world.getEnvironment();
         
         if (environment == World.Environment.NORMAL) {
-        	return "Normal";
+        	return WorldEnum.NORMAL;
         } else if (environment == World.Environment.NETHER) {
-        	return "Nether";
+        	return WorldEnum.NETHER;
         } else if (environment == World.Environment.THE_END) {
-        	return "TheEnd";
+        	return WorldEnum.THE_END;
         } else {
-        	return "Unknown";
+        	return WorldEnum.UNKNOWN;
         }
     }
 }
