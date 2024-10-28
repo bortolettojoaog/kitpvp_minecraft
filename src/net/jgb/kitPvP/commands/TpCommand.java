65255304453b0990e12d99df7629336aeada1c67
João G. Bortoletto <jgb3532@hotmail.com>
2024-10-28 12:34:56 -0300

    feat: instantiate util classes

--- src/net/jgb/kitPvP/commands/TpCommand.java
+++ src/net/jgb/kitPvP/commands/TpCommand.java
@@ -4,7 +4,22 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import net.jgb.kitPvP.Main;
+import net.jgb.kitPvP.utils.Environment;
+import net.jgb.kitPvP.utils.Message;
+import net.jgb.kitPvP.utils.languages.Language;
+
 public class TpCommand implements CommandExecutor {
+	
+	private Message messageUtils;
+	private Environment environmentUtils;
+	private Language language;
+	
+	public TpCommand() {
+		this.messageUtils = Main.getUtils().messageUtils();
+		this.environmentUtils = Main.getUtils().environmentUtils();
+		this.language = Main.getLanguage();
+	}
 
 	@Override
 	public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
